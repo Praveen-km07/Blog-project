0af56c2778eaabf4bd51b11e7a7940fa6c0b40c0
Praveen-km07 <testingudemyprojects.gmail.com>
2023-03-21 20:02:23 +0100

    section-3

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
-export const Home = () => {
+const home = () => {
   return (
     <div className='mb-20'>
-        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, welcome to my Blog</h1>
-        <p className='mx-auto leading-relaxed text-base mb-4'>Non sit aute laboris dolore aliqua fugiat irure in non eu eiusmod. Et aute nisi occaecat voluptate nulla qui ut labore commodo dolore nulla dolor. Mollit nostrud ullamco est qui ullamco non do consectetur fugiat sit officia aliquip excepteur. Ex deserunt amet anim aliqua ex proident mollit laboris. Ex eiusmod eu voluptate ipsum laboris aliquip ex aliquip aute exercitation occaecat nostrud ex non. Minim ullamco quis Lorem qui deserunt voluptate.</p>
-        <p className='mx-auto leading-relaxed text-base mb-4'>Non sit aute laboris dolore aliqua fugiat irure in non eu eiusmod. Et aute nisi occaecat voluptate nulla qui ut labore commodo dolore nulla dolor. Mollit nostrud ullamco est qui ullamco non do consectetur fugiat sit officia aliquip excepteur. Ex deserunt amet anim aliqua ex proident mollit laboris. Ex eiusmod eu voluptate ipsum laboris aliquip ex aliquip aute exercitation occaecat nostrud ex non. Minim ullamco quis Lorem qui deserunt voluptate.</p>
-    
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello welcome to my blog</h1>
+         <p className='mx-auto leading-relaxed text-base mb-4'>Id ad laborum minim est excepteur incididunt voluptate sint est tempor in. Deserunt cupidatat aliquip adipisicing incididunt deserunt duis et nulla eu dolor esse. Consectetur cupidatat laboris ex fugiat mollit Lorem do minim incididunt dolor sit sunt. Minim laborum exercitation dolore aute reprehenderit ea tempor aute mollit nulla. Ad officia id aute culpa minim minim. Sint labore excepteur consectetur non proident sint consequat incididunt commodo sint consequat reprehenderit dolor quis.
+
+Deserunt tempor commodo anim esse duis officia ipsum aute ullamco. Est velit mollit in fugiat irure reprehenderit. Pariatur qui enim esse adipisicing. Adipisicing reprehenderit deserunt aute laborum veniam excepteur cupidatat ex nulla consequat ex ea culpa et.
+
+Excepteur sit magna sunt consequat quis cillum minim do velit in laboris. Minim voluptate laboris velit id nulla laboris ullamco. Deserunt laborum mollit dolor irure dolore. Eu pariatur tempor ad in incididunt.</p>
+         <p className='mx-auto leading-relaxed text-base mb-4'>Lorem ipsum Id ad laborum minim est excepteur incididunt voluptate sint est tempor in. Deserunt cupidatat aliquip adipisicing incididunt deserunt duis et nulla eu dolor esse. Consectetur cupidatat laboris ex fugiat mollit Lorem do minim incididunt dolor sit sunt. Minim laborum exercitation dolore aute reprehenderit ea tempor aute mollit nulla. Ad officia id aute culpa minim minim. Sint labore excepteur consectetur non proident sint consequat incididunt commodo sint consequat reprehenderit dolor quis.
+
+Deserunt tempor commodo anim esse duis officia ipsum aute ullamco. Est velit mollit in fugiat irure reprehenderit. Pariatur qui enim esse adipisicing. Adipisicing reprehenderit deserunt aute laborum veniam excepteur cupidatat ex nulla consequat ex ea culpa et.
+
+Excepteur sit magna sunt consequat quis cillum minim do velit in laboris. Minim voluptate laboris velit id nulla laboris ullamco. Deserunt laborum mollit dolor irure dolore. Eu pariatur tempor ad in incididunt.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Id ad laborum minim est excepteur incididunt voluptate sint est tempor in. Deserunt cupidatat aliquip adipisicing incididunt deserunt duis et nulla eu dolor esse. Consectetur cupidatat laboris ex fugiat mollit Lorem do minim incididunt dolor sit sunt. Minim laborum exercitation dolore aute reprehenderit ea tempor aute mollit nulla. Ad officia id aute culpa minim minim. Sint labore excepteur consectetur non proident sint consequat incididunt commodo sint consequat reprehenderit dolor quis.
+
+Deserunt tempor commodo anim esse duis officia ipsum aute ullamco. Est velit mollit in fugiat irure reprehenderit. Pariatur qui enim esse adipisicing. Adipisicing reprehenderit deserunt aute laborum veniam excepteur cupidatat ex nulla consequat ex ea culpa et.
+
+Excepteur sit magna sunt consequat quis cillum minim do velit in laboris. Minim voluptate laboris velit id nulla laboris ullamco. Deserunt laborum mollit dolor irure dolore. Eu pariatur tempor ad in incididunt.</p>
     </div>
   )
 }
 
-export default Home;
+export default home
