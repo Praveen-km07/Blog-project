49a4d0125d5c2c4dc4c3be707d7cabe7518485ab
Praveen-km07 <testingudemyprojects.gmail.com>
2023-03-21 22:54:13 +0100

    rewriting the comments endpoints

--- server.js
+++ server.js
@@ -11,25 +11,45 @@ const PORT =process.env.PORT || 8000;
 //function of express.It parses incoming JSON payload
 app.use(express.json({extended:false}));
 
-app.get('/api/articles/:name',async (req,res)=>{
+const withDB =async(operations,res)=>{
     try{
-    const articleName=req.params.name;
-    const client= await MongoClient.connect('mongodb+srv://testingudemyprojects:wJDd43uXzGpVmHSQ@cluster0.0qqdtj3.mongodb.net/test');
-    const db = client.db("mernblog");
-    const articlesInfo =await db.collection('articles').findOne({name:articleName});
-    res.status(200).json(articlesInfo);
-    client.close();
-    }
-    catch(error){
-        res.status(500).json({message:"Error connecting to database",error})
-    }
+        
+        const client= await MongoClient.connect('mongodb+srv://testingudemyprojects:wJDd43uXzGpVmHSQ@cluster0.0qqdtj3.mongodb.net/test');
+        const db = client.db("mernblog");
+        await operations(db);
+        client.close();
+        }
+        catch(error){
+            res.status(500).json({message:"Error connecting to database",error})
+        }
+}
+
+app.get('/api/articles/:name',async (req,res)=>{
+
+    withDB(async (db)=> {
+        const articleName =req.params.name;
+        const articlesInfo =await db.collection("articles").findOne({name:articleName});
+        res.status(200).json(articlesInfo);
+    },res)
+    
 })
 
-app.post('/api/articles/:name/add-comments',(req,res)=>{
+app.post('/api/articles/:name/add-comments',async (req,res)=>{
     const {username,text}=req.body
     const articleName=req.params.name
-    articlesInfo[articleName].comments.push({username,text})
-    res.status(200).send(articlesInfo[articleName]);
+
+    withDB(async (db)=>{
+        const articlesInfo = await db.collection("articles").findOne({name:articleName});
+        await db.collection('articles').updateOne({name:articleName},{
+            $set : {
+                comments:articlesInfo.comments.concat({username,text}),
+            },
+        });
+        const updateArticleInfo =await db.collection('articles').findOne({name:articleName})
+        res.status(200).json(updateArticleInfo);
+    },res)
+    // articlesInfo[articleName].comments.push({username,text})
+    // res.status(200).send(articlesInfo[articleName]);
 })
 
 app.listen(PORT,()=>{
