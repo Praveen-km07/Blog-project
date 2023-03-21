dde73739dfb513e4a0571096934c1a8eb985128d
Praveen-km07 <testingudemyprojects.gmail.com>
2023-03-21 20:46:54 +0100

    adding comments and route parameter

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const { response } = require('express');
 const express = require('express');
 const app =express();
 const PORT =process.env.PORT || 8000;
+const articlesInfo ={
+    "learn-react":{
+        comments:[],
+    },
+    "learn-node":{
+        comments:[],
+    },
+    "my-thoughts-on-learning-react":{
+        comments:[],
+    }
+}
 
 //Intialise middleware
 //we use to have to install body parser but now it is a built in middleware
 //function of express.It parses incoming JSON payload
 app.use(express.json({extended:false}));
 
-app.get('/',(request,response)=>{
-      response.send ("Hello World");
-});
-
-
-app.post('/',(reqest,response)=>{
-    response.send(`Hello ${reqest.body.name}`);
+app.post('/api/articles/:name/add-comments',(req,res)=>{
+    const {username,text}=req.body
+    const articleName=req.params.name
+    articlesInfo[articleName].comments.push({username,text})
+    res.status(200).send(articlesInfo[articleName]);
 })
 
 app.listen(PORT,()=>{
